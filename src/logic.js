5ab4db2563537e040301af772907620b897b3f19
KTAGit <121195747+KTAGit@users.noreply.github.com>
2025-11-22 20:49:14 -0500

    Add helper functions for storing projects and todo items

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,5 @@
 
+
 export const projectStorage = []
 export const todoStorage = [] 
 
@@ -14,22 +15,33 @@ export const todoStorage = []
 
 // Represents a todo item assigned to a specific project.
 export class TodoCreator {
-    constructor( title, description, duedate, priority, parentID ) {
+    constructor( title, description, duedate, priority, status, parentID ) {
         this.title = title
         this.description = description
         this.duedate = duedate
         this.priority = priority
+        this.status = status
         this.parentID = parentID
     }
 }
 
 
+// Adds a new project object to the projectStorage array.
+function storeProject(project) {
+    projectStorage.push(project)
+}
+
+
+// Adds a new todo item to the todoStorage array.
+function storeTodo(todo) {
+    todoStorage.push(todo)
+}
 
 
 const pro1 = new ProjectCreator("Personal")
 const todo1 = new TodoCreator("Do homework", "Math homework needs to be completed", "8/5/2025", "High", pro1.id)
 
+storeTodo(todo1)
 
-console.log(pro1)
+console.log(todoStorage)
 
-console.log(todo1)
